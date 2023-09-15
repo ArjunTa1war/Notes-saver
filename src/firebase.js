23d4ca965cea2854b0b3c2c3571e217abf9230f5
Arjun Talwar <79759270+ArjunTa1war@users.noreply.github.com>
2023-09-16 02:51:02 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -6,12 +6,12 @@ import { getFirestore,collection } from "firebase/firestore"
 
 const firebaseConfig = {
   apiKey: "none",
-  authDomain: "react-notes-714a8.firebaseapp.com",
-  projectId: "react-notes-714a8",
-  storageBucket: "react-notes-714a8.appspot.com",
-  messagingSenderId: "428633443033",
-  appId: "1:428633443033:web:65dc7f51a5342e74a3ccc8",
-  measurementId: "G-XTDL8N6NVH"
+  authDomain: "hello",
+  projectId: "hello",
+  storageBucket: "hello",
+  messagingSenderId: "hello",
+  appId: "hello",
+  measurementId: "hello"
 };
 
 const app = initializeApp(firebaseConfig);
